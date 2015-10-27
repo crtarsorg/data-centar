c6505d2452fa0684a55b1302d7990ee3d3e13e0f
FestinaI <festina.ismajli123@gmail.com>
2015-10-27 16:38:31 +0100

    added mongo document structure

--- importing_manager.py
+++ importing_manager.py
@@ -1,5 +1,13 @@
 #coding=utf-8
 from csv import reader
+import cyrtranslit
+from flask_pymongo import MongoClient
+
+# Instantiate mongo database
+mongo = MongoClient()
+
+# Create mongo database
+db = mongo.datacentar
 
 class ImportingManager():
 
@@ -33,10 +41,31 @@ class ImportingManager():
         pass
 
     def data_importer_of_municipality_novi_beograd(self):
-        #data = reader(open("myfile.csv", "r"), delimiter=",")
-        pass
+        data = reader(open("myfile.csv", "r"), delimiter=",")
 
-    def build_mongo_docment_str(self):
-        json_doc = {}
+
+    def build_mongo_docment_structure(self, muncipality, kategorija_roditelj, roditelj_broj, class_number, opis, prihodi_vudzeta, sopstveni_prihodi, donacije, ostali):
+
+        ukupno = prihodi_vudzeta + sopstveni_prihodi + donacije + ostali
+        json_doc = {
+            "opstina": {
+                "cyrilic": muncipality,
+                "latin": cyrtranslit.to_latin(muncipality, "sr")
+            },
+            "kategorijaRoditelj": {
+                "opis": kategorija_roditelj,
+                "broj": roditelj_broj
+            },
+            "klasifikacijaBroj": class_number,
+            "opis": {
+                "latin": cyrtranslit.to_latin(opis, "sr"),
+                "cyrilic": opis.strip()
+            },
+            "prihodiVudzeta": prihodi_vudzeta,
+            "sopstveniPrihodi": sopstveni_prihodi,
+            "donacije": donacije,
+            "ostali": ostali,
+            "ukupno": ukupno
+        }
 
         return json_doc
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/endrit/Desktop/dev/DataCentar/venv"
+set -gx VIRTUAL_ENV "/Users/festina/Desktop/Development/DataCentar/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

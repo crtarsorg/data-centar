118ee2250f4c5bebc41a305165d49c0b2298fa55
FestinaI <festina.ismajli123@gmail.com>
2015-11-03 15:15:01 +0100

    Implemented test cases for Novi Beograd municipality

--- importer/test.py
+++ importer/test.py
@@ -1,12 +1,33 @@
+#coding=utf-8
 import unittest
+from importing_manager import mongo
 
 class RashodiImportingTestCases(unittest.TestCase):
 
     def setUp(self):
         pass
 
-    def test_upper_case(self):
-      self.assertEqual('foo'.upper(), 'FOO')
+    def test_novi_beograd_municipality(self):
+
+        self.asserts_for_parent_categories_elements("Novi Beograd", "Расходи за запослене", 7)
+        self.asserts_for_parent_categories_elements("Novi Beograd", "Коришћење услуга и роба", 6)
+        self.asserts_for_parent_categories_elements("Novi Beograd", "Донације дотације и трансфери", 2)
+        self.asserts_for_parent_categories_elements("Novi Beograd", "Социјално осигурање и социјална заштита", 1)
+        self.asserts_for_parent_categories_elements("Novi Beograd", "Остали расходи", 3)
+        self.asserts_for_parent_categories_elements("Novi Beograd", "Средства резерве", 2)
+        self.asserts_for_parent_categories_elements("Novi Beograd", "Основна средства", 3)
+
+    def asserts_for_parent_categories_elements(self, municipality, parent_category, expected_value):
+
+        result = mongo.datacentar.opstine.find(
+            {
+                "opstina.latin": municipality,
+                "kategorijaRoditelj.opis.cyrilic": parent_category
+            }
+        ).count()
+
+        self.assertEqual(result, expected_value)
+
 
 class PrihodiImportingTestCases(unittest.TestCase):
     pass

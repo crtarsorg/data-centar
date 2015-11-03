31d892ddb7a79435264d9987cd9350556e1853a5
FestinaI <festina.ismajli123@gmail.com>
2015-11-03 15:28:03 +0100

    Implemented test cases for Zvezdara municipality

--- importer/test.py
+++ importer/test.py
@@ -17,6 +17,18 @@ class RashodiImportingTestCases(unittest.TestCase):
         self.asserts_for_parent_categories_elements("Novi Beograd", "Средства резерве", 2)
         self.asserts_for_parent_categories_elements("Novi Beograd", "Основна средства", 3)
 
+    def test_zvezdara_municipality(self):
+        self.asserts_for_parent_categories_elements("Zvezdara", "РАСХОДИ ЗА ЗАПОСЛЕНЕ", 6)
+        self.asserts_for_parent_categories_elements("Zvezdara", "КОРИШЋЕЊЕ РОБА И УСЛУГА", 6)
+        self.asserts_for_parent_categories_elements("Zvezdara", "ОТПЛАТА КАМАТА", 1)
+        self.asserts_for_parent_categories_elements("Zvezdara", "ДОНАЦИЈЕ ,ДОТАЦИЈЕ И ТРАНСФЕРИ", 2)
+        self.asserts_for_parent_categories_elements("Zvezdara", "ОСНОВНА СРЕДСТВА", 3)
+        self.asserts_for_parent_categories_elements("Zvezdara", "ПРАВА ИЗ СОЦИЈАЛНОГ ОСИГУРАЊА", 1)
+        self.asserts_for_parent_categories_elements("Zvezdara", "ОСТАЛИ РАСХОДИ", 1)
+        self.asserts_for_parent_categories_elements("Zvezdara", "Порези, таксе, казне наметнуте од власти", 3)
+        self.asserts_for_parent_categories_elements("Zvezdara", "ОТПЛАТА ГЛАВНИЦЕ", 1)
+        self.asserts_for_parent_categories_elements("Zvezdara", "РЕЗЕРВА", 2)
+
     def asserts_for_parent_categories_elements(self, municipality, parent_category, expected_value):
 
         result = mongo.datacentar.opstine.find(

4c354c76310ecd6c2cc9ca41c518b67ae86cb6df
georges <georges@opendatakosovo.org>
2015-11-05 02:18:08 +0100

    Removed one of the images

--- app/mod_landing_page/views.py
+++ app/mod_landing_page/views.py
@@ -5,6 +5,6 @@ mod_landing_page = Blueprint('landing_page', __name__, static_url_path='static')
 
 @mod_landing_page.route('/', methods=['GET'])
 def index():
-    bg_id = random.randint(1, 8)
+    bg_id = random.randint(1, 7)
     return render_template('mod_landing_page/index.html', bg_id=str(bg_id))
 

2ac170e64b736a4809ffb2f4b89d14b061b6d789
georges <georges@opendatakosovo.org>
2015-11-05 02:14:31 +0100

    implemented random landing page image loader

--- app/mod_landing_page/views.py
+++ app/mod_landing_page/views.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+import random
 
 mod_landing_page = Blueprint('landing_page', __name__, static_url_path='static')
 
 @mod_landing_page.route('/', methods=['GET'])
 def index():
-    return render_template('mod_landing_page/index.html')
+    bg_id = random.randint(1, 8)
+    return render_template('mod_landing_page/index.html', bg_id=str(bg_id))
 

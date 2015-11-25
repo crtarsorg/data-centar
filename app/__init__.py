4c3f1b27c8c695d0be6f1ce7c5ecf5a9cd425da3
endrit <endriti.92@gmail.com>
2015-11-25 03:28:01 +0100

    comment added

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ def create_app():
     # Here we  create flask instance
     app = Flask(__name__)
 
+    # Allow cross-domain access to API.
     cors = CORS(app, resources={r"/api/*": {"origins": "http://datacentar.io"}})
 
     # Load application configurations

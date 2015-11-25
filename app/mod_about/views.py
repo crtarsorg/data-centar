c96ce8004839176e1eab09ce447f486347b7f019
mihajlo <mihajlo.andjelic@crta.rs>
2015-11-25 11:00:37 +0100

    route /about is working

--- app/mod_about/views.py
+++ app/mod_about/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-mod_about = Blueprint('about', __name__, static_url_path='static')
+mod_about = Blueprint('about', __name__, url_prefix='/about')
 
 @mod_about.route('/', methods=['GET'])
 def index():

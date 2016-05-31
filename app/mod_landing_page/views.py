3ed6ad404b06329c09fad68236d33d7eddd3910e
Partin <partin.imeri@gmail.com>
2016-05-31 08:28:51 +0200

    Added Serbian timeline translation.

--- app/mod_landing_page/views.py
+++ app/mod_landing_page/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 import random
 from app.utils.option import Option
 
@@ -6,7 +6,7 @@ mod_landing_page = Blueprint('landing_page', __name__, static_url_path='static')
 
 @mod_landing_page.route('/', methods=['GET'])
 def index():
-    return render_template('mod_landing_page/index.html', lang_code='en')
+    return redirect("/en", code=302)
 
 @mod_landing_page.route('/<lang_code>', methods=['GET'])
 def root():

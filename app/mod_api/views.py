8bf2971cd19598f6e4e2aab9cbb9d66b9699d981
ferideadili <feride_adili@hotmail.com>
2017-01-13 00:36:32 +0100

    api fix url and css

--- app/mod_api/views.py
+++ app/mod_api/views.py
@@ -47,4 +47,9 @@ def aggregated_classifications():
     return Response(response=json_util.dumps(json_resp), status=200, mimetype="application/json")
 
 
+@mod_api.route('/spisak-opstina-za-klasifikacija-broj', methods=['POST']) # List of municipalities for given class. number
+def list_of_municipalities():
+    query_params = json.loads(request.data)
+    json_resp = DataProvider().calculate_sum_of_expenditure_types(query_params)
 
+    return Response(response=json_util.dumps(json_resp), status=200, mimetype="application/json")
